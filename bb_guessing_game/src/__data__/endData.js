2441e771b8f7c06d6f7bd11b4c26e7569162b9fb
Colby Peyton <colby.peyton@gmail.com>
2020-09-11 18:24:50 -0500

    Updated end status gif. End screen is now responsive

--- bb_guessing_game/src/__data__/endData.js
+++ bb_guessing_game/src/__data__/endData.js
@@ -1,7 +1,7 @@
 const endGifs = [
   'https://media.giphy.com/media/xT0GqssRweIhlz209i/giphy.gif',
   'https://media.giphy.com/media/iowmvjVUnDFGU/giphy.gif',
-  'https://media.giphy.com/media/NcToAw9vkn8go/giphy.gif',
+  'https://media.giphy.com/media/3o6gEgkb5xqAyMw5Og/giphy.gif',
   'https://media.giphy.com/media/26BRQaiZM0IeyoJfa/giphy.gif'
 ]
 
